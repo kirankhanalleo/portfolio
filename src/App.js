ed58bf064aa9e2e3c36099b442e25240fb6414f8
Kiran Khanal <84300572+kirankhanalleo@users.noreply.github.com>
2022-10-17 20:09:54 +0545

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,17 +1,15 @@
 import React from "react";
 import "./App.css";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Contact from "./Contact";
 import HomeScreen from "./HomeScreen";
 function App() {
   return (
     <div className="app">
-      <Router>
         <Routes>
           <Route exact path="/portfolio" element={<HomeScreen />} />
           <Route exact path="/portfolio/contact" element={<Contact />} />
         </Routes>
-      </Router>
     </div>
   );
 }

6013fe4832579909959f608391923b54a100d528
Kiran Khanal <84300572+kirankhanalleo@users.noreply.github.com>
2022-10-17 19:51:46 +0545

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "./App.css";
-import { HashRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Contact from "./Contact";
 import HomeScreen from "./HomeScreen";
 function App() {
@@ -8,7 +8,7 @@ function App() {
     <div className="app">
       <Router>
         <Routes>
-          <Route exact path="/" element={<HomeScreen />} />
+          <Route exact path="/portfolio" element={<HomeScreen />} />
           <Route exact path="/contact" element={<Contact />} />
         </Routes>
       </Router>
